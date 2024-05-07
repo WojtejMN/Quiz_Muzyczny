b64c6b0a0f16cf97ef7621c389fdb6634c667fe6
WojtejMN <maciekn9@onet.pl>
2024-05-07 12:34:30 +0200

    Initial Commit

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def index():
+    return render_template('index.html', name='Gość')
+@app.route('/templates')
+def podstrona1():
+    return render_template('podstrona1.html', name='Gość')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
